737455d73c9b8def100cbc82d446a0302192a11c
Dag Heyman <dag@yubico.com>
2017-08-10 10:16:29 +0200

    update

--- game_of_chance.c
+++ game_of_chance.c
@@ -55,6 +55,86 @@ int main() {
         printf("[Name: %s]\n", player.name);
         printf("[You have %u credits] -> ", player.credits);
         scanf("%d", &choice);
-
+        
+        if((choice < 1) || (choice > 7))
+            printf("\n[!!] The number %d is an invalid selection.\n\n", choice);
+        else if (choice < 4) {
+            if (choice != last_game) {
+                if (choice == 1)
+                    player.current_game = pick_a_number;
+                else if (choice == 2)
+                    player.current_game = dealer_no_match;
+                else
+                    player.current_game = find_the_ace;
+                last_game = choice;
+            }
+            play_the_game();
+        }
+        else if (choice == 4)
+            show_highscore();
+        else if (choice == 5) {
+            printf("\nChange user name\n");
+            printf("\nEnter your new name:\n");
+            input_name();
+            printf("Your name has been changed.\n\n");
+        }
+        else if (choice == 6) {
+            printf("\nYour account has been reset with 100 credits.\n\n");
+            player.credits = 100;
+        }
     }
+    update_player_data();
+    printf("\nThanks for playing! Bye.\n");
 }
+
+// Read player data for current uid from file. Returns -1
+// if unable to find player data for the current uid.
+
+int get_player_data() {
+    int fd, uid, read_bytes();
+    struct user entry;
+
+    uid = getuid();
+
+    fd = open(DATAFILE, O_RDONLY);
+    if(fd == -1) // Can't open file
+        return -1;
+    read_bytes = read(fd, &entry, sizeof(struct user)); // Read first chunk
+    while(entry.uid != uid && read_bytes > 0) {
+        read_bytes = read(fd, &entry, sizeof(struct user));
+    }
+    close(fd);
+    if (read_bytes < sizeof(struct user))
+        return -1
+    else
+        player = entry;
+    return 1;
+}
+
+// User registration function
+// Register new player and appent it to the file.
+
+void register_new_player() {
+    int fd;
+
+    printf("-=-={ New Player Registration }=-=-\n");
+    printf("Enter your name: ");
+    input_name();
+
+    player.uid = getuid();
+    player.highscore = player.credits = 100;
+
+    fd = open(DATAFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+    if (fd == -1)
+        fatal("in register_new_player() while opening file");
+    write(fd, &player, sizeof(struct user));
+    close(fd);
+
+    printf("\nWelcome to the Game of Chance %s \n", player.name);
+    printf("You have been given %u credits. \n", player.credits);
+
+}
+
+// Writes the current player data to file.
+TODO: keep working here.
+
--- provision.sh
+++ provision.sh
@@ -2,9 +2,12 @@
 
 sudo apt-get update -qq && apt-get upgrade -qq
 
-# Install development dependencies
-# for both Python 2 and Python 3
 sudo apt-get install -qq \
     vim
     gcc
+    git
 
+# Fix dotfiles
+cd ~ 
+git clone https://github.com/dagheyman/.dotfiles.git
+.dotfiles/setup.sh
